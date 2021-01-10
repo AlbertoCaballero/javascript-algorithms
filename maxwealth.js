cb67b6e2eea1e7cf895a576e7194b6a1aa52de79
AlbertoCaballero <albertocaballero80@outlook.com>
2021-01-10 00:27:17 -0600

    Reformat

--- maxwealth.js
+++ maxwealth.js
@@ -1,14 +1,14 @@
-accounts = [[1,2,3],[4,5,6],[7,8,9]]
+accounts = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
 console.log(accounts)
-console.log(Math.max(...accounts.map(x => x.reduce((a,b) => a + b))))
+console.log(Math.max(...accounts.map(x => x.reduce((a, b) => a + b))))
 console.log(max_wealth(accounts))
 
 function max_wealth(accounts) {
   return Math.max(
     ...accounts.map(
       x => x.reduce(
-        (a,b) => a + b
+        (a, b) => a + b
       )
     )
   )
