6d1eaca10ca87764a7c9ca9b3690077de74610bd
AlbertoCaballero <albertocaballero80@outlook.com>
2021-02-08 12:23:57 -0600

    Sorting

--- sorting.js
+++ sorting.js
@@ -1,9 +1,8 @@
+less = (a, b) => a-b
+more = (a, b) => b-a
 
-less = (a, b) => { return a-b; };
-more = (a, b) => { return b-a; };
-
-items = [5, 6, 8, 12, 15, 2, 65, 6, 8, 12, 15, 2, 6];
-console.log(items);
-console.log(items.sort(less));
-console.log(items.sort(more));
+items = [5, 6, 1, 8, 12, 15, 2, 65, 6, 8, 12, 15, 2, 6]
+console.log(items)
+console.log(items.sort(less))
+console.log(items.sort(more))
 
