e96f04013b31f46dec9cf62f8de4d838fe38e5d9
AlbertoCaballero <albertocaballero80@outlook.com>
2021-03-27 14:50:31 -0600

    Better prime

--- prime.js
+++ prime.js
@@ -1,19 +1,17 @@
-const numbers = [...Array(20).keys()]
+const numbers = [...Array(100).keys()]
 
 function isPrime(n) {
-  if (n <= 1) return 'Neither';
-  if (n > 1) {
-    for (let i = 2; i < n; i++) {
-      if (n % i == 0)
-        return 'Not Prime';
-    }
-    return 'Prime';
-  } else {
-    return 'Not Prime';
+  if (n <= 1) return null;
+  if (n == 2) return true;
+  if (n % 2 == 0) return false;
+  for (let i = 2; i < n; i++) {
+    if (n % i == 0)
+      return false;
   }
+  return true;
 }
 
 for (var num in numbers) {
-  console.log(num, isPrime(num))
+  if (isPrime(num)) console.log(num);
 }
 
