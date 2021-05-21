b44d4c0979bab48185e31fb54b2076724823aad0
AlbertoCaballero <albertocaballero80@outlook.com>
2021-05-21 02:08:38 -0500

    Reformating

--- linked-lists.ts
+++ linked-lists.ts
@@ -1,39 +1,39 @@
 class ListNode {
-    val: number
-    next: ListNode | null
-    constructor(val?: number, next?: ListNode | null) {
-        this.val = (val === undefined ? 0 : val)
-        this.next = (next === undefined ? null : next)
-    }
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+  }
 }
 
 // Should return the sum of two integers stores by character in reverse order in a linked list
 function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
-    return null;
+  return null;
 }
 
 // Print the entirety of the list values in order
 function printList(list: ListNode) {
-    let current: ListNode = list;
-    while (current != null) {
-        console.log(current.val);
-        current = current.next;
-    }
+  let current: ListNode = list;
+  while (current != null) {
+    console.log(current.val);
+    current = current.next;
+  }
 }
 
 // reverse the linked list
 function reverse(list: ListNode): ListNode {
-    let prev = null;
-    let current = list;
-    let next = null;
-    while (current != null) {
-        next = current.next;
-        current.next = prev;
-        prev = current;
-        current = next;
-    }
-    current = prev;
-    return current
+  let prev = null;
+  let current = list;
+  let next = null;
+  while (current != null) {
+    next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+  current = prev;
+  return current
 }
 
 // Define a simple linked list
