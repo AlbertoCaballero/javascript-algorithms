fb4f5c6410a0fffefb10792667d4b800bc571209
AlbertoCaballero <albertoCaballero80@outlook.com>
2026-07-10 03:18:10 -0600

    Word break, partially solved

---
+++ word-break.ts
@@ -0,0 +1,23 @@
+function wordBreak(s: string, wordDict: string[]): boolean {
+    let results: boolean[] = [];
+
+    results.push(check(s, wordDict));
+
+    wordDict = wordDict.sort((a, b) => a.localeCompare(b));
+    results.push(check(s, wordDict.toReversed()));
+
+    return results.includes(true);
+}
+
+function check(word: string, dict: string[]): boolean {
+    let holder = word;
+
+    dict.forEach((word) => {
+        while (holder.includes(word)) {
+            holder = holder.replaceAll(word, " ");
+        }
+        holder = holder.replaceAll(" ", "");
+    });
+
+    return holder === "";
+}
